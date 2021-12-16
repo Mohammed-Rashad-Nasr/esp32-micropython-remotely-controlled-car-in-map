d823825f1de3f5126c5b77cded6efe557a9f82ce
Mohammed Rashad <84274861+Mohammed-Rashad-Nasr@users.noreply.github.com>
2021-12-16 13:39:45 +0200

    6th trial

--- main.py
+++ main.py
@@ -3,25 +3,11 @@ from machine import Pin, PWM
 from time import sleep
 import network
 import socket
-#import mpu6050
-#import math
-#rd = 180/math.pi
-#i2c = I2C(scl=Pin(22), sda =Pin(21))
-#mpu = mpu6050.accel(i2c)
-#values = mpu.get_values()
-#def map(x,in_min,in_max,out_min,out_max):
-#    return int((x-in_min)*(out_max-out_min)/(in_max-in_min) + out_min)
-#x_ang = map(values["AcX"],256,402,-90,90)
-#y_ang = map(values["AcY"],256,402,-90,90)
-#z_ang = map(values["AcZ"],256,402,-90,90)
-#x = rd * (math. atan2(-y_ang,-z_ang)+math.pi)
-#{ = rd * (math.atan2(-x_ang,-z_ang)+math.pi)
-#z = rd * (math.atan2(-y_ang,-x_ang)+math.pi)
-#print(x)
+
 x=0
 y=0
 heading=0
-
+end =False  
 ssid = 'Control'
 password = '123456789'
 ap = network.WLAN(network.AP_IF)
@@ -86,77 +72,77 @@ def step () :
     global x
     global y
     global heading
-    if enc >2 :
-        sleep(1)
-        stop()
-        enc = 0
-        if heading == 0 :
-            y=y+1
-        elif heading == 90 :
-            x=x+1
-        elif heading == 180:
-            y=y-1
-        elif heading ==-90 :
-            x=x-1
-        conn.send(str(x)+str(y))
-        print(str(x)+str(y)) 
-    else:
+    while enc < 313:
         forward()
+    stop()
+    enc = 0
+    if heading == 0 :
+        y=y+1
+    elif heading == 90 :
+        x=x+1
+    elif heading == 180:
+        y=y-1
+    elif heading ==-90 :
+        x=x-1
+    conn.send(str(x)+str(y))
+    print(str(y)+str(x)) 
 
 def rightturn () :
     global enc
     global heading
-    if enc > 313 :
-        stop()
-        enc = 0
-        if (heading +90)==270 :
-            heading = -90 
-        elif (heading+90)==360 :
-            heading=0
-        else :
-            heading=heading+90
-  
-    else:
+    while enc < 313:
         right()
+    stop()
+    if (heading +90)==270 :
+        heading = -90 
+    elif (heading+90)==360 :
+        heading=0
+    else :
+        heading=heading+90
+         
 
 def movey (ypos):
+    i = 0
     for i in range(ypos):
         step()
     
         
 def movex (xpos):
-    for i in range(xpos):
+    j = 0
+    for j in range(xpos):
         step()
     
-    
+   
 def moveto (xx,yy):
-    movey(yy)
-    sleep(0.5)
-    stop()
-    sleep(0.5)
-    rightturn()
-    sleep(0.5)
-    stop()
-    sleep(0.5)
-    movex(xx)
-    stop()
-#enable1 = PWM(Pin(18), frequgncy)  
-#enable = PWM(Pin(27), frequency)  
-#dc_motor = DCMotor(pinn2, pinn1, enable)
-#dc_motor= DCMotor(pinn2, pinn1, enable, 350, 1023)
-#dc_motor1 = DCMotor(pinn4, pinn3, enable1)
-#dc_motor1 = DCMotor(pinn4, pinn3, enable1, 350, 1023)
+    global end
+    global enc
+    if xx == 0:
+        movey(yy)
+    else:
+        movey(yy)
+        enc = 0
+        sleep(0.5)
+        stop()
+        sleep(0.5)
+        rightturn()
+        enc = 0
+        sleep(0.5)
+        stop()
+        sleep(0.5)
+        movex(xx)
+    end = True
+
 enc1 = Pin(33, Pin.IN)
 enc1.irq(encfunc,Pin.IRQ_RISING)
 enc2 = Pin(32, Pin.IN)
 enc2.irq(enc2func,Pin.IRQ_RISING)
 flag = ''
 flag2=False
-while True:
+while True :
     if mode == 'm' :
    
         while flag2 == False :
-        #global enc
+            #global enc
             conn, addr = s.accept()
             data = conn.recv(1024)
             data = str(data)
@@ -165,7 +151,7 @@ while True:
             enc = 0
             flag2=True
         while flag2 == True : 
-        #global enc
+            #global enc
         
             if flag == 'F': 
        
@@ -210,5 +196,8 @@ while True:
                     right()  
               
     else:
-        moveto(x2,y2)
-        break
+        if end == False :
+            moveto(x2,y2)
+        else
+            break
+        
